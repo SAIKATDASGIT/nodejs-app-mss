f8acd375c9944764ab9b527d5f5db5ee58fb2bed
saikat_das_git <90147135+SAIKATDASGIT@users.noreply.github.com>
2021-12-10 10:54:39 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.108.59.54:9000/',
+  serverUrl: 'http://192.168.56.102:82/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
